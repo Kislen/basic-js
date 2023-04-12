580707f9bb5af1eb201045c1c3b25edc3afc420b
Kisliankou Viachaslau <kislenkov.vs@gmail.com>
2023-04-12 22:11:50 +0300

    feat: add chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,49 +5,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  arr:[],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return chainMaker.arr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    if (value === undefined) { value = " ";}
+      chainMaker.arr.push(`( ${value} )`);
+      return chainMaker;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (!chainMaker.arr.includes(chainMaker.arr[position - 1])) {
+      chainMaker.arr = []; 
+      throw new Error("You can't remove incorrect link!");
+    }
+      chainMaker.arr.splice(position - 1, 1);
+      return chainMaker;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    chainMaker.arr.reverse();
+    return chainMaker;
   },
   finishChain() {
-    let arr = [];
-    let chainMaker  = {
-      getLength() {
-        return arr.length;
-      },
-      addLink(value) {
-        if (value === undefined) { value = " ";}
-        arr.push(`( ${value} )~~`);
-        return chainMaker;
-      },
-      removeLink(position) {
-        if (arr[position] === undefined) { throw new Error("You can't remove incorrect link!");}
-        arr = arr.slice(position, position + 1);
-        return chainMaker;
-      },
-      reverseChain() {
-        arr = arr.reverse();
-      },
-      finishChain() {
-        let str = arr.join();
-        arr = [];
-        return str;
-      }
-    }
+      let str = chainMaker.arr.join('~~');
+      chainMaker.arr = [];
+      return str;
   }
 }
+
 module.exports = {
   chainMaker
 };
