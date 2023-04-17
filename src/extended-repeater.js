e9630368f91b113e2b25afd8a157afde4f6666e6
Kisliankou Viachaslau <kislenkov.vs@gmail.com>
2023-04-17 23:29:22 +0300

    feat: add a part of extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let ar = []
+  const objWithSpecificCoercion = {
+    [Symbol.toPrimitive]: hint => hint !== 'number' ? 'STRING_OR_DEFAULT' : 'NUMBER'
+  };
+   for (let i = 0; i < options.additionRepeatTimes; i++) {
+    if (options.addition === null) options.addition ='null';
+    else options.addition = options.addition.toString();
+    ar[i] = options.addition;
+   }
+   let add = ar.join(`${options.additionSeparator}`);
+   
+   let arr = [];
+   if (str === null) str ='null';
+   else str = str.toString();
+   for (let i = 0; i < options.repeatTimes; i++) {
+    arr[i] = str + add;
+   }
+   options.separator === undefined? options.separator = '+':options.separator = options.separator;
+   return arr.join(`${options.separator}`);
 }
 
 module.exports = {
